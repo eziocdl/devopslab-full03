f9e5b3a02be771b276b9927f4395735e8016982c
Ezio Cintra de Lima <eziocdl@gmail.com>
2022-05-23 15:32:16 -0300

    Bugg Added

--- app.py
+++ app.py
@@ -9,5 +9,19 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Hello World - Ezio"
 
+  @app.route("/")
+def pagina_inicial():
+    return "Laboratório Pipeline DevOps"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+  
+
 if __name__ == '__main__':
     app.run()
